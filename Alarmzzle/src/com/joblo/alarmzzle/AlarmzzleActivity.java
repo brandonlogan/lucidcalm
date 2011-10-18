e7f46eae5c635db4edde073421aa9f5fa293a1f0
Brandon Logan <johnbrandonlogan@gmail.com>
2011-10-17 23:56:09 -0500

    first project commit

---
+++ Alarmzzle/src/com/joblo/alarmzzle/AlarmzzleActivity.java
@@ -0,0 +1,13 @@
+package com.joblo.alarmzzle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmzzleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
